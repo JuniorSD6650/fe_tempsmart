40081b03dd5d4dc5abc193d41fcab8a2b6176d61
JuniorSD6650 <fancy6650@gmail.com>
2024-07-27 22:55:32 -0500

    first commit

---
+++ src/components/ActivityList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function ActivityList() {
+    return (
+        <div>
+            <h2>Lista de Actividades</h2>
+            {/* Aquí se mostrará la lista de actividades */}
+        </div>
+    );
+}
+
+export default ActivityList;
---
+++ src/components/AddActivity.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function AddActivity() {
+    return (
+        <div>
+            <h2>Añadir Actividad</h2>
+            {/* Formulario para añadir una nueva actividad */}
+        </div>
+    );
+}
+
+export default AddActivity;
